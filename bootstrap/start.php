0d4b326b66ba4c3a71e4d15b9fea0b3af7647b6f
ramblingsofadev <dbyoung2@gmail.com>
2014-12-11 10:32:41 -0600

    Rearranged start sequence

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -12,9 +12,9 @@ require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../configs/php.php";
 
 $application = require_once __DIR__ . "/../configs/application.php";
-$application->start();
 $request = Request::createFromGlobals();
 $application->getIoCContainer()->bind("RDev\\HTTP\\Request", $request);
+$application->start();
 /** @var Router $router */
 $router = $application->getIoCContainer()->makeShared("RDev\\Routing\\Router");
 require_once __DIR__ . "/../configs/routing.php";

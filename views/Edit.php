84db8c54abdd914a76b4cb69df9b70572cd3c937
ramblingsofadev <dbyoung2@gmail.com>
2014-12-14 18:19:44 -0600

    Updated template

--- views/Edit.php
+++ views/Edit.php
@@ -4,7 +4,10 @@
 <h2>Editing This Project</h2>
 <h3>Template Contents</h3>
 <p>
-    To change the contents of each page, change the templates in the <b>views</b> directory.  If you want to create <i>Builders</i> for your templates, add them to <b>app/project/views/builders</b>.  Then, register each <i>Builder</i> to the appropriate template in <b>app/project/views/bootstrappers/Builders.php</b>.
+    To change the contents of each page, change the templates in the <b>views</b> directory.
+</p>
+<p>
+    If you want to create <i>Builders</i> for your templates, add them to <b>app/project/views/builders</b>.  Then, register each <i>Builder</i> to the appropriate template in <b>app/project/views/bootstrappers/Builders.php</b>.
 </p>
 <h3>Routes</h3>
 <p>

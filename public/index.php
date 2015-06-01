4eb386853643659cb468b52390939258c3ae0445
ramblingsofadev <dbyoung2@gmail.com>
2015-05-31 22:59:59 -0500

    Removed .htaccess

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This page acts as a single point-of-entry for all pages on a website
- * All requests are redirected via the .htaccess file to this file
  * RDev's router then routes the request to the appropriate controller
+ * Read the installation docs at www.rdevphp.com for more information
  */
 require_once __DIR__ . "/../bootstrap/http/start.php";

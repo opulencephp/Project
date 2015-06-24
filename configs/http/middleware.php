d909bed24153f28703b489f4fffbf1ad296f11a2
ramblingsofadev <dbyoung2@gmail.com>
2015-06-24 08:57:06 -0500

    Removed comments

--- configs/http/middleware.php
+++ configs/http/middleware.php
@@ -6,13 +6,6 @@ use Project\HTTP\Middleware\Session;
 use Project\HTTP\Middleware\CheckCSRFToken;
 
 return [
-    /**
-     * ----------------------------------------------------------
-     * Middleware to be run on every route
-     * ----------------------------------------------------------
-     *
-     * List any HTTP middleware you'd like here
-     */
     Session::class,
     CheckCSRFToken::class
 ];

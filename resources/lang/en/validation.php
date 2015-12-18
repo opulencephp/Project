548039ec905076bc14c85af644f11a3f77faabde
opulencephp <dbyoung2@gmail.com>
2015-12-18 13:47:41 -0600

    Added some more error messages

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -5,7 +5,12 @@
  * ----------------------------------------------------------
  */
 return [
+    "callback" => "The :field field is invalid",
     "email" => "The :field must be a valid email address",
     "equals" => "The :field field did not match the expected value",
+    "equalsField" => "The :field did not match the :other field",
+    "in" => "The :field field is invalid",
+    "notIn" => "The :field field is invalid",
+    "regex" => "The :field field is invalid",
     "required" => "The :field field is required"
 ];

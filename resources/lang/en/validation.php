28312e7ef326797f5c145c24c42cd68beaf43910
opulencephp <dbyoung2@gmail.com>
2015-12-18 14:51:17 -0600

    Added some more error messages

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -10,7 +10,9 @@ return [
     "equals" => "The :field field did not match the expected value",
     "equalsField" => "The :field did not match the :other field",
     "in" => "The :field field is invalid",
+    "integer" => "The :field field is not a valid integer",
     "notIn" => "The :field field is invalid",
+    "numeric" => "The :field field is must be a number",
     "regex" => "The :field field is invalid",
     "required" => "The :field field is required"
 ];

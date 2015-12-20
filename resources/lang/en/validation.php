1ca55ffe33dd6e2b27e15f7d780e519bfa38bcfa
opulencephp <dbyoung2@gmail.com>
2015-12-20 17:16:37 -0600

    Added some more error messages

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -5,12 +5,18 @@
  * ----------------------------------------------------------
  */
 return [
+    "alpha" => "The :field field must only contain alphabet characters",
+    "alphaNumeric" => "The :field field must only contain alpha-numeric characters",
+    "between" => "The :field field must be between :min and :max",
     "callback" => "The :field field is invalid",
     "email" => "The :field must be a valid email address",
     "equals" => "The :field field did not match the expected value",
     "equalsField" => "The :field did not match the :other field",
     "in" => "The :field field is invalid",
     "integer" => "The :field field is not a valid integer",
+    "ipAddress" => "The :field field is not a valid IP address",
+    "max" => "The :field field must be less than :max",
+    "min" => "The :field field must be greater than :min",
     "notIn" => "The :field field is invalid",
     "numeric" => "The :field field is must be a number",
     "regex" => "The :field field is invalid",

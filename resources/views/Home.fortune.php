e32714ecec8d391bcb4ad449ce3e067249abffec
opulencephp <dbyoung2@gmail.com>
2016-09-26 23:27:32 -0500

    Updated branding

--- resources/views/Home.fortune.php
+++ resources/views/Home.fortune.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta name="viewport" content="width=1024"/>
+        <meta name="viewport" content="initial-scale=1" />
         {{! charset("utf-8") !}}
         {{! pageTitle($title) !}}
         {{! css("http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,300,400,700") !}}
         {{! css($css) !}}
-        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png?v=1.0" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png?v=1.0" sizes="16x16">
     </head>
     <body>
         <main class="main">

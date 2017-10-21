9ed186042cbbb23ecff3e15221bd2032e8134855
opulencephp <dbyoung2@gmail.com>
2017-10-21 16:36:27 -0500

    Removed the migration that created the executed migration table - that's now down automatically in the repository

--- src/Infrastructure/Databases/Migrations/CreateExecutedMigrationTable.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Project\Infrastructure\Databases\Migrations;
-
-use Opulence\Framework\Databases\Migrations\CreateExecutedMigrationTable as BaseMigration;
-
-class CreateExecutedMigrationTable extends BaseMigration
-{
-    // Don't do anything unless you want to override the built-in migration
-}
